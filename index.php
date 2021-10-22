a4d4d87a1761a82b2eee4c5d85d5b67d840f4095
NimbleFIsh <karpov2001ura@mail.ru>
2021-10-22 23:24:19 +0300

    add Card

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 <?php
+    // $ = file_get_contents('https://gis-api.admlr.lipetsk.ru/api/v1/roads/regionallist'); // Дороги
+    // $ = file_get_contents('https://gis-api.admlr.lipetsk.ru/api/v1/roads/federallist'); // Трассы
     
--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Адресс PHP сервера: https://mpu2021.herokuapp.com/
+    // адресс для дорог: https://gis-api.admlr.lipetsk.ru/api/v1/roads/regionallist
+    // адресс для трасс: https://gis-api.admlr.lipetsk.ru/api/v1/roads/federallist
+    //result.features.filter(data => typeof(data.properties.name) === 'string' && data.properties.name.includes('Липецк, ') && data);
+    const d = document;
+    const map = d.getElementById('map');
+    map.width = window.innerWidth;
+    map.height = window.innerHeight;
 
-    
 });
