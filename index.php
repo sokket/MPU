866f8a496785fb78b4b31d373d507cc72b5b76b9
NimbleFIsh <karpov2001ura@mail.ru>
2021-10-23 15:04:50 +0300

    remove PHP

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-    // $ = file_get_contents('https://gis-api.admlr.lipetsk.ru/api/v1/roads/regionallist'); // Дороги
-    // $ = file_get_contents('https://gis-api.admlr.lipetsk.ru/api/v1/roads/federallist'); // Трассы
-    
