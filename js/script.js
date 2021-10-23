43d9742cc8e4ac73ca0195ddbbcefcee88e896ac
NimbleFIsh <karpov2001ura@mail.ru>
2021-10-23 13:04:04 +0300

    add road from garage, buatify and optimizated code

--- js/script.js
+++ js/script.js
@@ -4,10 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     // адресс для трасс: https://gis-api.admlr.lipetsk.ru/api/v1/roads/federallist
     // result.features.filter(data => typeof(data.properties.name) === 'string' && data.properties.name.includes('Липецк, ') && data);
 
-    let map = L.map('map').setView([52.5886, 39.5662], 13);
-    map.setView([52.5886, 39.5662], 12);
-    L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png',
-        {attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+    let map = L.map('map').setView([52.5990, 39.5679], 12);
+    L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
 
     const coordGarage = [ // Координаты гаражей
         [52.6055795, 39.52249936], // Липецк
@@ -16,22 +14,12 @@ document.addEventListener('DOMContentLoaded', () => {
         [52.60272342632745, 38.48057850930626]  // Елец
     ]; 
 
-    function renderGarages(coords) {        
-        L.marker(coords, {icon: L.icon({
-            iconUrl: './garage.svg',
-            iconSize: [38, 95],
-            iconAnchor: [22, 94],
-            popupAnchor: [-3, -76]
-        })}).addTo(map);
-    } 
-
-
-
-    coordGarage.forEach(el => L.marker(el).addTo(map)); // Рендер точек гаражей
+    // Функция для рендера иконок гаражей
+    const renderGarages = coords => L.marker(coords,{icon:L.icon({iconUrl:'./garage.svg',iconSize:[30,80],popupAnchor:[-3,-76]})}).addTo(map);
+    const renderRoad = coords => map.fitBounds(L.polyline(coords, {color: 'blue'}).addTo(map).getBounds()); // Функция для рендера дорог
     
-    const renderRoad = coords => map.fitBounds(L.polyline(coords, {color: 'blue'}).addTo(map).getBounds());
-    
-    roads.forEach(road => renderRoad(road.geometry.coordinates));
+    coordGarage.forEach(el => renderGarages(el)); // Отрисовка гаражей
+    roads.forEach(road => renderRoad(road.geometry.coordinates)); // Отрисовка дорог
 
     /*
     Код для отрисовки линий по координатам
