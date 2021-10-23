c662e8a5a5d8f4b8cd5fde2741dc971368f86dd1
NimbleFIsh <karpov2001ura@mail.ru>
2021-10-23 09:33:44 +0300

    test another map code

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
     // адресс для дорог: https://gis-api.admlr.lipetsk.ru/api/v1/roads/regionallist
     // адресс для трасс: https://gis-api.admlr.lipetsk.ru/api/v1/roads/federallist
     //result.features.filter(data => typeof(data.properties.name) === 'string' && data.properties.name.includes('Липецк, ') && data);
-    let map = L.map('map').setView([52.5886, 39.5662], 13);
-    map.setView([52.5886, 39.5662], 12);
-    // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-    const polyline = L.polyline([52.5886, 39.5662]);
+    // let map = L.map('map').setView([52.5886, 39.5662], 13);
+    // map.setView([52.5886, 39.5662], 12);
+    // // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+    // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    //     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    // }).addTo(map);
+    // const polyline = L.polyline([52.5886, 39.5662]);
 });
