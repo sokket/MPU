81badcf06e6cd494d592dde7928632bd53663268
xinik <sergey20160@gmail.com>
2021-10-23 20:11:32 +0300

    Refactor

--- consts.js
+++ consts.js
@@ -1,4 +1,4 @@
-const SERVERHOST = 'https:///api/v1/'; // Базовый адресс сервера
+const SERVERHOST = 'https://project.typex.one/api/v1/'; // Базовый адресс сервера
 
 const coordGarage = [ // Координаты гаражей
     [52.6055795, 39.52249936],              // Липецк
@@ -15,36 +15,3 @@ function sendReq(method, mode, callback) { // Функция для станда
 }
 
 let routes = []; // Массив для машин
-
-function getPicture(type) { // Определяет тип машину и подбирает картинку для неё
-    return 1;
-}
-
-function renderVihocles(coords, type) { // Рендерит на карте технику
-    L.marker(coords,{icon:L.icon({iconUrl:'./img/'+getPicture(type)+'.png',iconSize:[60,45],popupAnchor:[-3,-76]})}).addTo(map);
-}
-
-function refreshPosVehicles(e = false) { // Получение координат машин или запрос на новые данные
-    sendReq('GET', e !== false ? 'refreshRoutes' : 'locations', getCoords);
-    console.log('Запрос/Обновление позиций машин');
-}
-
-function getVehicles(data) { // Получает названия машин
-    data.forEach(el => routes[el.id].name = el.name);
-    console.log(routes); // Отладка
-}
-
-function getCoords(data) { // Получает текущие координаты машин
-    data.forEach(el => {
-        routes[el.id].coords = el.pos;
-        renderVihocles(el.pos);
-    });
-    console.log(routes); // Отладка
-}
-
-function renderRoadVehicles(data) { // Получает маршруты и их ID
-    data.forEach(el => routes[el.id] = map.fitBounds(L.polyline(el.points, {color: 'green'}).addTo(map).getBounds()));
-    console.log(routes); // Отладка
-    sendReq('GET', 'vehicles', getVehicles); // Получение маршрутов
-    refreshPosVehicles();
-}
--- script.js
+++ script.js
@@ -10,6 +10,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const renderRoad = coords => map.fitBounds(L.polyline(coords, {color: 'blue'}).addTo(map).getBounds()); // Функция для рендера дорог
     
     coordGarage.forEach(el => renderGarages(el)); // Отрисовка гаражей
+
+    // Рендерит на карте технику
+    const renderVehicles = (coords, type) => L.marker(coords,{icon:L.icon({iconUrl:'./img/' + getPicture(type) + '.png',iconSize:[60,45],popupAnchor:[-3,-76]})}).addTo(map);
+
     
     sendReq('GET', 'streets', roads => roads.forEach(road => renderRoad(road.geometry.coordinates))); // Получение и отрисовка дорог
 
@@ -17,7 +21,9 @@ document.addEventListener('DOMContentLoaded', () => {
         
     }
 
-    sendReq('GET', 'routes', renderRoadVehicles); // Получение маршрутов
+    refreshPosVehicles();
+
+    // sendReq('GET', 'routes', renderRoadVehicles); // Получение маршрутов
     
     // let server = new EventSource(SERVERHOST + 'locationsStream');
     
@@ -26,4 +32,32 @@ document.addEventListener('DOMContentLoaded', () => {
     // // позже заменить на: updateCurLoc(JSON.parse(msg.data));
 
     // server.onerror = err => console.error(err);
+
+    function getPicture(type) { // Определяет тип машину и подбирает картинку для неё
+        return 1;
+    }
+
+    function refreshPosVehicles(e = false) { // Получение координат машин или запрос на новые данные
+        sendReq('GET', e !== false ? 'refreshRoutes' : 'locations', getCoords);
+        console.log('Запрос/Обновление позиций машин');
+    }
+
+    function getVehicles(data) { // Получает названия машин
+        data.forEach(el => routes[el.id].name = el.name);
+        console.log(routes); // Отладка
+    }
+
+    function getCoords(data) { // Получает текущие координаты машин
+        data.forEach(el => {
+            // routes[el.id].coords = el.pos;
+            renderVehicles(el.pos);
+        });
+    }
+
+    function renderRoadVehicles(data) { // Получает маршруты и их ID
+        data.forEach(el => routes[el.id] = map.fitBounds(L.polyline(el.points, {color: 'green'}).addTo(map).getBounds()));
+        console.log(routes); // Отладка
+        sendReq('GET', 'vehicles', getVehicles); // Получение маршрутов
+        refreshPosVehicles();
+    }
 });
