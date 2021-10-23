a2382b69ed3bad48d2187c04e064d499dd2a0037
NimbleFIsh <karpov2001ura@mail.ru>
2021-10-23 14:16:18 +0300

    base SSE (not tested) and add webRequests for server, moved consts from another file

--- js/consts.js
+++ js/consts.js
@@ -1,4 +1,22 @@
-const roads = [
+const SERVERHOST = '/api/v1/';
+
+const coordGarage = [ // Координаты гаражей
+    [52.6055795, 39.52249936],              // Липецк
+    [52.6389125, 39.53994646],              // Липецк
+    [52.60469729740339, 38.48343237969811], // Елец
+    [52.60272342632745, 38.48057850930626]  // Елец
+];
+
+function sendReq(method, mode, callback) { // Функция для стандартного сетевого взаимодействия
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, SERVERHOST + mode, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.responseType = 'json';
+    xhr.addEventListener('readystatechange', () => xhr.readyState === xhr.DONE && callback(JSON.parse(xhr.response)));
+    xhr.send();
+}
+
+const roads = [ // Координаты всех дорог
     {
         "type": "Feature",
         "geometry": {
--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Адресс PHP сервера: https://mpu2021.herokuapp.com/
-    // адресс для дорог: https://gis-api.admlr.lipetsk.ru/api/v1/roads/regionallist
-    // адресс для трасс: https://gis-api.admlr.lipetsk.ru/api/v1/roads/federallist
-    // result.features.filter(data => typeof(data.properties.name) === 'string' && data.properties.name.includes('Липецк, ') && data);
-
-    let map = L.map('map').setView([52.5990, 39.5679], 12);
-    L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
-
-    const coordGarage = [ // Координаты гаражей
-        [52.6055795, 39.52249936], // Липецк
-        [52.6389125, 39.53994646], // Липецк
-        [52.60469729740339, 38.48343237969811], // Елец
-        [52.60272342632745, 38.48057850930626]  // Елец
-    ]; 
+    let vehicels = [], map = L.map('map').setView([52.5990, 39.5679], 12);
+    L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map); // Задание слоя маски - обычный
 
     // Функция для рендера иконок гаражей
     const renderGarages = coords => L.marker(coords,{icon:L.icon({iconUrl:'./garage.svg',iconSize:[30,80],popupAnchor:[-3,-76]})}).addTo(map);
     const renderRoad = coords => map.fitBounds(L.polyline(coords, {color: 'blue'}).addTo(map).getBounds()); // Функция для рендера дорог
     
-    coordGarage.forEach(el => renderGarages(el)); // Отрисовка гаражей
-    roads.forEach(road => renderRoad(road.geometry.coordinates)); // Отрисовка дорог
+    coordGarage.forEach(el => renderGarages(el));                   // Отрисовка гаражей
+    roads.forEach(road => renderRoad(road.geometry.coordinates));   // Отрисовка дорог
 
-    /*
-    Код для отрисовки линий по координатам
+    function renderRoadVehicles(data) { // Получает маршруты и их ID
+        data.forEach(el => vehicels[el.id] = map.fitBounds(L.polyline(el.points, {color: 'green'}).addTo(map).getBounds()));
+        console.log(vehicels); // Отладка
+    }
 
-    let latlngs = [ [52.6169, 39.5767], [52.6046, 39.5960] ];
-    map.fitBounds(L.polyline(latlngs, {color: 'red'}).addTo(map).getBounds());
+    sendReq('GET', 'routes', renderRoadVehicles); // Получение маршрутов
 
-    */
+    let server = new EventSource(SERVERHOST + 'locationsStream');
+    server.open = () => console.log('SSE connected'); // Отладка
+    server.onmessage = msg => console.log(JSON.parse(msg.data));
+    // позже заменить на: updateCurLoc(JSON.parse(msg.data));
+
+    server.onerror = err => console.error(err);
 });
+
+/*
+Код для отрисовки линий по координатам
+
+let latlngs = [ [52.6169, 39.5767], [52.6046, 39.5960] ];
+map.fitBounds(L.polyline(latlngs, {color: 'red'}).addTo(map).getBounds());
+
+*/
