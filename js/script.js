a99e5775cb5cd32f836e5179d0e63e0dd3fc2aed
NimbleFIsh <karpov2001ura@mail.ru>
2021-10-23 14:56:58 +0300

    add REST API, move roads to server, add update data button, move more functions to consts.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let vehicels = [], map = L.map('map').setView([52.5990, 39.5679], 12);
+    const updBtn = document.getElementById('updatePos');
+    updBtn.addEventListener('click', refreshPosVehicles);
+
+    let routes = [], map = L.map('map').setView([52.5990, 39.5679], 12);
     L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map); // Задание слоя маски - обычный
 
     // Функция для рендера иконок гаражей
     const renderGarages = coords => L.marker(coords,{icon:L.icon({iconUrl:'./garage.svg',iconSize:[30,80],popupAnchor:[-3,-76]})}).addTo(map);
     const renderRoad = coords => map.fitBounds(L.polyline(coords, {color: 'blue'}).addTo(map).getBounds()); // Функция для рендера дорог
     
-    coordGarage.forEach(el => renderGarages(el));                   // Отрисовка гаражей
-    roads.forEach(road => renderRoad(road.geometry.coordinates));   // Отрисовка дорог
+    coordGarage.forEach(el => renderGarages(el)); // Отрисовка гаражей
+    
+    sendReq('GET', 'streets', roads => roads.forEach(road => renderRoad(road.geometry.coordinates))); // Получение и отрисовка дорог
 
-    function renderRoadVehicles(data) { // Получает маршруты и их ID
-        data.forEach(el => vehicels[el.id] = map.fitBounds(L.polyline(el.points, {color: 'green'}).addTo(map).getBounds()));
-        console.log(vehicels); // Отладка
+    function updateCurLoc(data) { // Обновляет координаты машин (передвигает машины)
+        
     }
 
     sendReq('GET', 'routes', renderRoadVehicles); // Получение маршрутов
 
     let server = new EventSource(SERVERHOST + 'locationsStream');
-    server.open = () => console.log('SSE connected'); // Отладка
-    server.onmessage = msg => console.log(JSON.parse(msg.data));
+    
+    server.open = () => updBtn.classList.remove('hide'); // Отладка
+    server.onmessage = msg => console.log(JSON.parse(msg.data)); // Отладка
     // позже заменить на: updateCurLoc(JSON.parse(msg.data));
 
     server.onerror = err => console.error(err);
