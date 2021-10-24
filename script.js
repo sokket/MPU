aa556a81155b9254e550bf3cbc9bef815a1f2970
NimbleFIsh <karpov2001ura@mail.ru>
2021-10-24 09:27:30 +0300

    deploy

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const updBtn = document.getElementById('updatePos'); // Получение кнопки для обновления
-    const AllColors = ['green', 'red', 'blue', 'orange', 'yellow', 'purple', 'grey', 'brown'];
-    let usedColors = [];
+    const countVehicels = document.getElementById('countVehicels'); // Получение ссылки на ползунок
+    const enableStreets = document.getElementById('useStreets'); // Получение 
+
     // Получение координат машин или запрос на новые данные
     const getPosVehicles = (e = false) => sendReq('GET', e !== false ? 'refreshRoutes' : 'locations', getCoords);
     updBtn.addEventListener('click', getPosVehicles); // Обработчик для кнопки обновить
 
     let map = L.map('map').setView([52.5990, 39.5679], 12); // Инициализация карты
-    L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map); // Задание слоя маски - обычный
-
-    // Функция для рендера иконок гаражей
+    L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map); // Задание слоя Array.from(маски.children.forEach(el => el.remove()))   // Функция для рендера иконок гаражей
     const renderGarages = coords => L.marker(coords,{icon:L.icon({iconUrl:'./img/garage.svg',iconSize:[30,80],popupAnchor:[-3,-76]})}).addTo(map);
     const renderRoad = (coords, color) => map.fitBounds(L.polyline(coords, {'color': color}).addTo(map).getBounds()); // Функция для рендера дорог
     
@@ -23,9 +22,17 @@ document.addEventListener('DOMContentLoaded', () => {
         return 1;
     }
     
-    function getVehicles(data) { // Получает названия машин
-        data.forEach(el => routes[el.id].name = el.name);
+    function clearMap() { // Очистка карты
+        const tmp31 = document.getElementsByClassName('leaflet-marker-pane')[0];
+        const tmp32 = document.getElementsByClassName('leaflet-overlay-pane')[0];
+
+        Array.from(tmp31.children).forEach(el => el.remove());
+        if(tmp32.childElementCount !== 0) Array.from(tmp32.children[0].children).forEach(el => el.remove());
     }
+
+    // function getVehicles(data) { // Получает названия машин
+    //     data.forEach(el => routes[el.id].name = el.name);
+    // }
 
     const getCoords = data => data.forEach(el => renderVehicles(el.pos)); // Получает текущие координаты машин   
 
@@ -36,17 +43,29 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function renderRoadVehicles(data) { // Получает маршруты и их ID
-        data.forEach(el1=>{
+        data.forEach(el1 => {
             let tmp = getRandomColor();
             el1.streets.forEach(e => L.polyline(e, {'color': tmp}).addTo(map))
         });
         getPosVehicles(); // Получение позиций машин
+        // updBtn.classList.remove('hide');
         // sendReq('GET', 'vehicles', getVehicles); // Получение наименований машин
     }
 
-    //sendReq('GET', 'streets', roads => roads.forEach(road => renderRoad(road.geometry.coordinates))); // Получение и отрисовка дорог
+    countVehicels.onchange = e => start(e.target.value, enableStreets.checked); // При изменении ползунка - перерендерить всё
+    enableStreets.onchange = e => start(countVehicels.value, e.target.checked); // При изменении ползунка - перерендерить всё
 
-    sendReq('GET', 'routes', renderRoadVehicles, 102); // Получение маршрутов
+    function start(count, mode) {
+        clearMap(); // Очистка карты
+
+        if (mode) sendReq('GET', 'streets', roads =>
+            roads.forEach(road => renderRoad(road.geometry.coordinates, 'blue'))); // Получение и отрисовка дорог
+
+        sendReq('GET', 'routes', renderRoadVehicles, count); // Получение маршрутов    
+    }
+
+    start(countVehicels.value, enableStreets.checked);
+    
     
     // function updateCurLoc(data) { // Обновляет координаты машин (передвигает машины)
         
@@ -54,7 +73,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // let server = new EventSource(SERVERHOST + 'locationsStream');
     
-    // server.open = () => updBtn.classList.remove('hide'); // Отладка
+    // server.open = () =>  // Отладка
     // server.onmessage = msg => console.log(JSON.parse(msg.data)); // Отладка
     // // позже заменить на: updateCurLoc(JSON.parse(msg.data));
 
